4f137a6cb0fe7585cbd57e7c0f09b33ff1403234
Ilnytska <olly.happyfly@gmail.com>
2021-12-17 12:41:54 +0200

    Update 02-video.js
    add solution of task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+function onPlay({ seconds }) {
+    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+}
+const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
+player.setCurrentTime(currentTime).then(function (value) {value = currentTime;
+  })
+.catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:  
+            break;
+    }
+});
+player.on('timeupdate', throttle(onPlay, 1000));
