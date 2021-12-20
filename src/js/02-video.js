7b805a90a671bf9141e1ad6a7c0b76cc9df42b6e
Ilnytska <olly.happyfly@gmail.com>
2021-12-20 23:36:07 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,14 +4,14 @@ const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 function onPlay({ seconds }) {
-    localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
 }
 const currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
-
+const parseTime = JSON.parse(currentTime);
 player
-    .setCurrentTime(currentTime)
+    .setCurrentTime(parseTime)
     .then(function (value) {
-        value = currentTime;
+        value = parseTime;
   })
 .catch(function(error) {
     switch (error.name) {
